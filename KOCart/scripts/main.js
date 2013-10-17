e5a5ba7ed1e4ddfae12ac097e5f13b73273e0f38
Moollihawkja <jlera@lm127372-jlera.jomax.paholdings.com>
2013-10-17 08:34:15 -0700

    Added jquery and $.address plugins

--- KOCart/scripts/main.js
+++ KOCart/scripts/main.js
@@ -1,4 +1,6 @@
-require(["../lib/ko_v2.3.0","../scripts/products"], function(ko, products) {
-	var $body = document.getElementsByTagName("body")[0];
-	ko.applyBindings(products, $body);
-});
+require(["../lib/jquery_v2.0.3.js", "../lib/address_v1.5.js", "../lib/ko_v2.3.0","../scripts/products"],
+	function($, address, ko, products) {
+		var $body = document.getElementsByTagName("body")[0];
+		ko.applyBindings(products, $body);
+	}
+);
